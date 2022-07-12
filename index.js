d1f445457e38f44159549f6e5d4be06682a7822b
Ortal Wikoff <owikoff@gmail.com>
2022-07-12 13:22:36 -0400

    updated index.js

--- index.js
+++ index.js
@@ -5,4 +5,123 @@ const util = require("util");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const teplate = require("./src/templateHtml"); 
+const html = require("./src/templateHtml"); 
+
+const writeFileAsync = util.promisify(fs.writeFile);
+const appendFileAsync = util.promisify(fs.appendFile);
+
+let teamArray = [];
+let teamstr = ``;
+
+// calls all the functions in one order
+async function main() {
+     try {
+          await prompt()
+          // for i to teamArray.length  => 
+
+          for (let i = 0; i < teamArray.length; i++) {
+               //template literal=``
+               teamstr = teamstr + html.genrateCard(teamArray[i]);
+          }
+
+          let finalHTML = html.generateHtml(teamstr)
+
+          //call generate function to generate the html template literal
+
+          writeFileAsync("./dist/index.html", finalHTML)
+
+          //console.log(teamstr)
+
+     } catch (err) {
+          return console.log(err);
+     }
+
+};
+
+async function prompt() {
+     let responseDone = "";
+     let response = "";
+     // prompt to collect input and use do while atleast one and do it number of times depending on the while condition
+     do {
+          try {
+                await inquirer.prompt([
+                    {
+                         type: "input",
+                         name: "name",
+                         message: "Employee's name: "
+                    },
+                    {
+                         type: "input",
+                         name: "id",
+                         message: "Employee's ID: "
+                    },
+                    {
+                         type: "input",
+                         name: "email",
+                         message: "Employee's email address: "
+                    },
+                    {
+                         type: "list",
+                         name: "role",
+                         message: "employee's role:",
+                         choices: [
+                              "Engineer",
+                              "Intern",
+                              "Manager"
+                         ]
+                    }
+               ]);
+
+               let response2 = ""
+               // if else statement
+
+               if (response.role === "Engineer") {
+                    response2 = await inquirer.prompt([{
+                         type: "input",
+                         name: "x",
+                         message: "Employee's github username?:",
+                    }, ]);
+                    //store the object and push
+                    const engineer = new Engineer(response.name, response.id, response.email, response2.x);
+                    teamArray.push(engineer);
+               } else if (response.role === "Intern") {
+                    response2 = await inquirer.prompt([{
+                         type: "input",
+                         name: "x",
+                         message: "What school is the employee attending?:",
+                    }, ]);
+                    //store the object and push
+                    const intern = new Intern(response.name, response.id, response.email, response2.x);
+                    teamArray.push(intern);
+               } else if (response.role === "Manager") {
+                    response2 = await inquirer.prompt([{
+                         type: "input",
+                         name: "x",
+                         message: "Employee's office number:",
+                    }, ]);
+                    //store the object and push
+                    const manager = new Manager(response.name, response.id, response.email, response2.x);
+                    teamArray.push(manager);
+               }
+          } catch (err) {
+               return console.log(err);
+          }
+          console.log(teamArray)
+          //need to prompt do you want to continue
+
+          responseDone = await inquirer.prompt([{
+               type: "list",
+               name: "finish",
+               message: "Do you want to continue?: ",
+               choices: [
+                    "Yes",
+                    "No"
+               ]
+          }, ]);
+
+          // console.log(responseDone.choices);
+          //the while parameter is saying continue running the code if the user selects "yes"
+     } while (responseDone.finish === "Yes");
+}
+//call function to run application on the server
+main();
--- src/templateHtml.js
+++ src/templateHtml.js
@@ -1,106 +1,105 @@
-const generateHtml = function (team) {
-    return `
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
-    <title>Team Profile Generator</title>
-
-</head>
-<body>
-        <h1>My Team</h1>
-
-    <div class="body">
- 
-<div class="card">
- <div class="card-header">
-     <h2></h2>  
-     <h2>Manager</h2>
-     <hr>
- </div>
-
- <div class="card-body">
-     <ul>
-         <li>ID:</li>
-         <li>Email:</li>
-         <li>Office Number:</li>
-     </ul>
- </div>
- </div>
- 
- <div class="card">
- <div class="card-header">
-     <h2></h2>  
-     <h2>Engineer</h2>
-     <hr>
- </div>
-
- <div class="card-body">
-     <ul>
-         <li>ID:</li>
-         <li>Email:</li>
-         <li>Github Username:</li>
-     </ul>
- </div>
-
- </div><div class="card">
- <div class="card-header">
-     <h2></h2>  
-     <h2>Intern</h2>
-     <hr>
- </div>
-
- <div class="card-body">
-     <ul>
-         <li>ID:</li>
-         <li>Email:</li>
-         <li>School:</li>
-     </ul>
- </div>
- </div> 
- 
-    </div>
-
-    ${team} 
-
-
-</body>
-
-</html>
-    `
+const generateHtml= function (teamstr){
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./style.css">
+        <title>Team Profile Generator</title>
+    
+    </head>
+    <body>
+            <h1>My Team</h1>
+    
+        <div class="body">
+     
+    <div class="card">
+     <div class="card-header">
+         <h2></h2>  
+         <h2>Manager</h2>
+         <hr>
+     </div>
+    
+     <div class="card-body">
+         <ul>
+             <li>ID:</li>
+             <li>Email:</li>
+             <li>Office Number:</li>
+         </ul>
+     </div>
+     </div>
+     
+     <div class="card">
+     <div class="card-header">
+         <h2></h2>  
+         <h2>Engineer</h2>
+         <hr>
+     </div>
+    
+     <div class="card-body">
+         <ul>
+             <li>ID:</li>
+             <li>Email:</li>
+             <li>Github Username:</li>
+         </ul>
+     </div>
+    
+     </div><div class="card">
+     <div class="card-header">
+         <h2></h2>  
+         <h2>Intern</h2>
+         <hr>
+     </div>
+    
+     <div class="card-body">
+         <ul>
+             <li>ID:</li>
+             <li>Email:</li>
+             <li>School:</li>
+         </ul>
+     </div>
+     </div> 
+     
+        </div>
+    
+    </div> 
+    
+    </body>
+    
+    </html>`
 }
 
-//generate the card employee = employee object
+//generate the card emp = employee object
 
-const generateCard = function (employee) {
+const generateCard = function(emp) {
     //if else statement
-    let roleTheInfo;
+    let roleInfo;
 
-    if (employee.title === "Manager") {
-        roleTheInfo = `Office Number: ${employee.officeNumber}`
-    } else if (employee.title === "Engineer") {
-        roleTheInfo = `Github Username: ${employee.github}`
-    } else if (employee.title === "Intern") {
-        roleTheInfo = `School: ${employee.school}`
+    if (emp.title === "Manager") {
+        roleInfo = `Office Number: ${emp.officeNumber}`
+    } else if (emp.title === "Engineer") {
+        roleInfo = `Github Username: ${emp.github}`
+    } else if (emp.title === "Intern") {
+        roleInfo = `School: ${emp.school}`
     }
 
     return `<div class="card">
 <div class="card-header">
-<h2>${employee.name}</h2>  
-<h2> ${employee.title}</h2>
+<h2>${emp.name}</h2>  
+<h2>${emp.title}</h2>
 <hr>
 </div>
 <div class="card-body">
 <ul>
-    <li>ID: ${employee.id}</li>
-    <li>Email: ${employee.email}</li>
-    <li>${roleTheInfo}</li>
+    <li>ID: ${emp.id}</li>
+    <li>Email: ${emp.email}</li>
+    <li>${roleInfo} </li>
 </ul>
 </div>
 </div>`
 }
 
-exports.generateHtml = generateHtml
+exports.generateHtml = generateHtml;
+exports.genrateCard = generateCard; 
+
